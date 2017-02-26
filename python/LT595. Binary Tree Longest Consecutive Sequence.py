8b02dc73e51f9fdf3516f6ad3042a87ac5568429
EowynREN <EowynREN@163.com>
2017-02-25 23:12:06 -0800

    same as finding max in array; use global max to store the max val (careful for the discontinuous - make left (or right) seq 0)

---
+++ python/LT595. Binary Tree Longest Consecutive Sequence.py
@@ -0,0 +1,65 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # @param {TreeNode} root the root of binary tree
+    # @return {int} the length of the longest consecutive sequence path
+
+    # 思路: 与在一个数组里找最大一样
+    #      遍历一颗树, 用一个成员变量来储存最大值
+    #      这里唯一需要注意的地方是
+    #                          当不再连续的时候,需要把当前子树的seq的值变成0
+
+    def __init__(self):
+        self.max_seq = 0
+
+    def longestConsecutive(self, root):
+        # Write your code here
+        seq = self.helper(root)
+        return self.max_seq
+
+    def helper(self, node):
+        if not node:
+            return 0
+
+        left_seq = self.helper(node.left)
+        right_seq = self.helper(node.right)
+
+        if node.left and left_seq != 0:
+            # 不再连续的时候,需要把当前子树的seq的值变成0
+            if node.left.val - 1 != node.val:
+                self.max_seq = max(self.max_seq, left_seq)
+                left_seq = 0
+            # 仍然连续, 更新最大值, 把当前的父节点也算上
+            else:
+                self.max_seq = max(self.max_seq, left_seq + 1)
+
+        if node.right and right_seq != 0:
+            # 不再连续的时候,需要把当前子树的seq的值变成0
+            if node.right.val - 1 != node.val:
+                self.max_seq = max(self.max_seq, right_seq)
+                right_seq = 0
+            # 仍然连续, 更新最大值, 把当前的父节点也算上
+            else:
+                self.max_seq = max(self.max_seq, right_seq + 1)
+
+        seq = max(left_seq, right_seq)
+        return seq + 1
+
+t0 = TreeNode(2)
+t1 = TreeNode(3)
+t0.right = t1
+
+t2 = TreeNode(2)
+t1.left = t2
+
+t3 = TreeNode(1)
+t2.left = t3
+
+
+s = Solution()
+print s.longestConsecutive(t0)
