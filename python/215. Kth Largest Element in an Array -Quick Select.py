5386e23d2cdb5271f0adc48b074036494398e420
EowynREN <EowynREN@163.com>
2017-04-05 16:26:55 -0700

    add more comments

--- python/215. Kth Largest Element in an Array -Quick Select.py
+++ python/215. Kth Largest Element in an Array -Quick Select.py
@@ -32,10 +32,13 @@ class Solution(object):
         return self.quickSelect(nums, 0, len(nums) - 1, len(nums) - k + 1)
 
     def quickSelect(self, nums, left, right, k):
+        # 如果找完了nums数组还是没是没找到，就返回最接近的一个（数组的最开头或者最末尾）
         if left == right:
             return nums[left]
 
         position = self.partition(nums, left, right)
+
+        # +1是因为数组是zero-based
         if position + 1 == k:
             return nums[position]
         elif position + 1 < k:
@@ -48,17 +51,30 @@ class Solution(object):
 
         # 因为不是取中间值作为pivot,不能够再从左右两边向里挪指针
         # 需要下面这种处理方法,将小于pivot的数挪到pivot左边,讲大于的数挪到右边
+
+        # 为什么是 left < right 而不是 <= ,
+        # 因为当跳出while循环的时候left = right
+        # 两个指针正好一起直到pivot的地方
         while left < right:
+
+            # 在最右面找到第一个比pivot小的数
             while left < right and nums[right] >= pivot:
                 right -= 1
 
             nums[left] = nums[right]
+
+            # 在最左面找到第一个比pivot大的数
             while left < right and nums[left] <= pivot:
                 left += 1
 
             nums[right] = nums[left]
 
+        # 最后把pivot放在一个位置
+        # 这是left = right,指向同一个位置
+        # 左边的值逗比它小， 右边的值都比它大
+        # 这里正好放置pivot
         nums[left] = pivot
+
         return left
 
 s = Solution()
