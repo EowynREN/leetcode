51057e9d631e35515b760a7bdfd7b8c83f6ebb25
EowynREN <EowynREN@163.com>
2020-12-13 18:33:11 -0800

    dp solution

---
+++ python/5. Longest Palindromic Substring.py
@@ -0,0 +1,48 @@
+#-*- coding:utf8 -*-
+#coding=utf-8
+
+# 动态规划的解法: dp[i][j表示子串i到j是不是palindrome
+#               dp[i][j] = dp[i + 1][j - 1] and s[i - 1] == s[j - 1]
+#               这里由于地推公式,i + 1要比i先算出来,所以这道题要从右到左扫
+# 时间复杂度: O(n^2)
+# 空间度咋读: O(n^2)
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        if not s:
+            return ""
+
+        n = len(s)
+        dp = [[False for j in xrange(n + 1)] for i in xrange(n + 1)]
+
+        # init:
+        for i in xrange(n + 1):
+            dp[i][i] = True
+
+        max_len = 0
+        start, end = 0, 0
+
+        # 倒着从串为开始找子串(从右到左),i指针在左边,j指针在右边,一直扩大到整个string
+        for i in xrange(n, 0, -1):
+            for j in xrange(i + 1, n + 1):
+                # 单个字符
+                if s[i - 1] == s[j - 1] and j - i < 2:
+                    dp[i][j] = True
+
+                # 当前subtring的最大子串是回文,且左右边缘的字符相同->也是回文
+                if s[i - 1] == s[j - 1] and dp[i + 1][j - 1]:
+                    dp[i][j] = True
+
+                # 更新最大回文子串开头结尾的指针
+                if dp[i][j] and j - i + 1 > max_len:
+                    max_len = j - i + 1
+                    start = i - 1
+                    end = j - 1
+        return s[start: end + 1]
+
+
+s = Solution()
+print s.longestPalindrome("cbbd")
